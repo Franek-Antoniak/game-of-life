77a516bb32a710eed672fb3b9b49854c5f8b3ad4
Paco <franekant123@gmail.com>
2022-08-09 23:59:11 +0200

    Gradle updated to version 7.2. Main functionality coded.

--- src/main/java/com/company/paco/game/of/life/Main/Main.java
+++ src/main/java/com/company/paco/game/of/life/Main/Main.java
@@ -1,7 +1,33 @@
 package com.company.paco.game.of.life.Main;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        // Create a Scanner object to read input from the keyboard
+        Scanner keyboard = new Scanner(System.in);
+        // Get n and seed from the user
+        System.out.print("Enter n: ");
+        int n = keyboard.nextInt();
+        System.out.print("Enter seed: ");
+        int seed = keyboard.nextInt();
+        // Set seed for random number generator
+        Random rand = new Random(seed);
+        // Create array of booleans named map
+        int[][] map = new int[n][n];
+        // Initialize map with random values
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = rand.nextBoolean() ? 1 : 0;
+            }
+        }
+        // Print map
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(map[i][j] == 1 ? "O" : " ");
+            }
+            System.out.println();
+        }
     }
 }
